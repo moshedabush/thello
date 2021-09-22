aa59d541c696b0c73b4eaa3da27f06319acaae67
Eran Danieli <eran.danieli@gmail.com>
2021-09-22 18:59:57 +0300

    folders in src,rootcmp,routes.js

---
+++ src/RootCmp.jsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+// import { Switch, Route } from 'react-router'
+// import routes from './routes'
+
+// export class RootCmp extends React.Component {
+
+//     render() {
+//         return (
+//             <h1>RootCmp</h1>
+//         )
+//     }
+// }
