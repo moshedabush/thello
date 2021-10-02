3caa4865c8693532569b36bbb81b2885ef1d7265
Eran Danieli <eran.danieli@gmail.com>
2021-10-02 21:23:31 +0300

    boardList- starting scss

--- src/pages/BoardList.jsx
+++ src/pages/BoardList.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { userService } from '../services/user.service';
 import { loadUsers } from '../store/user.actions';
 // import boardData from '../data/boardsData';
-import  boardsData from '../data/boards.json'
+import boardsData from '../data/boards.json';
 import { Link } from 'react-router-dom';
 import { AppHeader } from '../cmps/app-header';
 
@@ -12,13 +12,13 @@ class _BoardList extends React.Component {
     loggedUser: {},
     boards: [],
   };
-  
+
   async componentDidMount() {
     const loggedUser = await userService.getLoggedinUser();
     const boardIds = await this.getUserBoardsIds(loggedUser);
     const boards = await this.getBoards(boardIds);
-    this.setState({ loggedUser , boards });
-    console.log('this.state',this.state)
+    this.setState({ loggedUser, boards });
+    console.log('this.state', this.state);
   }
 
   getUserBoardsIds = (loggedUser) => {
@@ -28,26 +28,47 @@ class _BoardList extends React.Component {
   };
 
   getBoards = (boardIds) => {
-    let filteredBoards = boardIds.map((id)=> boardsData.find(({_id})=>_id===id))
+    let filteredBoards = boardIds.map((id) =>
+      boardsData.find(({ _id }) => _id === id)
+    );
     return filteredBoards;
-    
   };
-  
 
   render() {
-   
-    const { boards } = this.state;
+    const { boards, loggedUser } = this.state;
     if (!boards) return <div>Loading</div>;
-    console.log('boards in render',boards)
+    console.log('boards in render', boards);
     return (
-      <div>
-        <AppHeader/>
-        <h1>Board List</h1>
-        {boards.map((board, idx) => (
-          <Link to={`board/${board._id}`} key={idx}>
-            <span>{board.title}</span>
-          </Link>
-        ))}
+      <div className={'board-list-container'}>
+        <AppHeader />
+        <div className={'board-list-recently-viewed'}>
+          <div>
+            <h4> 🕒 Recently viewed</h4>
+          </div>
+        </div>
+
+        <ul className={'user-board-list'}>
+          <div className={'your-workspaces'}>
+            <h4> YOUR WORKSPACES</h4>
+            <div>{loggedUser.username} Workspaces</div>
+          </div>
+          {boards.map((board, idx) => (
+            <div className={'board-list-card'}>
+              <Link to={`board/${board._id}`} key={idx}>
+                <div
+                  className={'board-list-card'}
+                  style={{
+                    backgroundColor: board.style.backgroundColor
+                      ? board.style.backgroundColor
+                      : 'green',
+                  }}>
+                  {console.log('board.style', board.style)}
+                  <div className={'board-list-title'}>{board.title}</div>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </ul>
       </div>
     );
   }
